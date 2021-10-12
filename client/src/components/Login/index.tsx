825d2820c6f6fbc25e5a1dc4ae1090c70c05dca2
Crgio <randaledomman1@gmail.com>
2021-10-12 02:32:18 -0300

    feat(client): create Login component

---
+++ client/src/components/Login/index.tsx
@@ -0,0 +1,146 @@
+import './Login.scss';
+import { useState } from 'react';
+import {
+	signUp,
+	signIn,
+	LoginOption,
+	LoginState,
+	evalInputRegex,
+} from './utils';
+
+export const Login = () => {
+	const [loginOption, setLoginOption] = useState<LoginOption>(
+		LoginOption.SignUp,
+	);
+	const [loginState, setLoginState] = useState<LoginState>(
+		LoginState.NotLogged,
+	);
+	const [usernameInputEval, setUsernameInputEval] = useState<boolean>(false);
+
+	const evalUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const evaluate = evalInputRegex.test(event.currentTarget.value);
+		if (evaluate !== usernameInputEval) {
+			setUsernameInputEval(evaluate);
+		}
+	};
+
+	const selectSignUp = () => setLoginOption(LoginOption.SignUp);
+	const selectSignIn = () => setLoginOption(LoginOption.SignIn);
+	const switchLoginOption = () => {
+		if (loginOption === LoginOption.SignIn) selectSignUp();
+		if (loginOption === LoginOption.SignUp) selectSignIn();
+		setLoginState(LoginState.NotLogged);
+	};
+
+	const handleSubmit = (event: React.SyntheticEvent) => {
+		setLoginState(LoginState.Loading);
+		event.preventDefault();
+		if (loginOption === LoginOption.SignUp) {
+			signUp(event)
+				?.then((response) => {
+					if (response.status === 201) {
+						setLoginState(LoginState.Succeed_SignUp);
+					}
+				})
+				.catch((error) => {
+					if (error.response.status === 400) {
+						setLoginState(LoginState.Failed_ExistingAccount);
+					} else if (error.response.status === 503) {
+						setLoginState(LoginState.Failed_NoService);
+					}
+				});
+		}
+		if (loginOption === LoginOption.SignIn) {
+			signIn(event)
+				?.then((response) => {
+					if (response.status === 200) {
+						setLoginState(LoginState.Succeed_SignIn);
+					}
+				})
+				.catch((error) => {
+					if (error.response.status === 404) {
+						setLoginState(LoginState.Failed_NonExistingAccount);
+					} else if (error.response.status === 503) {
+						setLoginState(LoginState.Failed_NoService);
+					}
+				});
+		}
+	};
+
+	if (loginOption === LoginOption.Unselected)
+		return (
+			<div className="Login_select-container">
+				<button onClick={selectSignUp}>Sign Up</button>
+				<span>or</span>
+				<button onClick={selectSignIn}>Sign In</button>
+			</div>
+		);
+	else if (loginOption === LoginOption.SignIn)
+		return (
+			<>
+				<form className="Login_SignIn-container" onSubmit={handleSubmit}>
+					<div>
+						<label htmlFor="username">username</label>
+						<input
+							type="text"
+							name="username"
+							id="username"
+							autoFocus
+							required
+						/>
+					</div>
+					<button type="submit">start playing!</button>
+					<span>
+						or <button onClick={switchLoginOption}>create an account</button>
+					</span>
+				</form>
+				{loginState !== LoginState.NotLogged && (
+					<span className="Login_State">
+						<strong>{loginState}</strong>
+					</span>
+				)}
+			</>
+		);
+	else
+		return (
+			<>
+				<form className="Login_SignUp-container" onSubmit={handleSubmit}>
+					<div>
+						<label htmlFor="username">username</label>
+						<input
+							onChange={evalUsername}
+							minLength={7}
+							maxLength={10}
+							type="text"
+							name="username"
+							id="username"
+							className={`${usernameInputEval ? '_valid' : '_unvalid'}`}
+							autoFocus
+							required
+						/>
+					</div>
+					<div>
+						<label htmlFor="nickname">nickname</label>
+
+						<input
+							maxLength={20}
+							type="text"
+							name="nickname"
+							id="nickname"
+							required
+						/>
+					</div>
+					<button type="submit">Use this</button>
+					<span>
+						or{' '}
+						<button onClick={switchLoginOption}>use an existing account</button>
+					</span>
+				</form>
+				{loginState !== LoginState.NotLogged && (
+					<span className="Login_State">
+						<strong>{loginState}</strong>
+					</span>
+				)}
+			</>
+		);
+};
---
+++ client/src/components/Login/utils.ts
@@ -0,0 +1,53 @@
+import { SyntheticEvent } from 'react';
+import axios from 'axios';
+
+export const signUp = (event: SyntheticEvent) => {
+	const target = event.target as typeof event.target & {
+		username: {
+			value: string;
+		};
+		nickname: {
+			value: string;
+		};
+	};
+	console.log(target.username, target.nickname);
+
+	if (target.username.value && target.nickname.value) {
+		return axios.post('../auth/signup', {
+			username: target.username.value,
+			nickname: target.username.value,
+		});
+	}
+};
+
+export const signIn = (event: SyntheticEvent) => {
+	const target = event.target as typeof event.target & {
+		username: {
+			value: string;
+		};
+	};
+	if (target.username.value) {
+		return axios.post('../auth/signin', {
+			username: target.username.value,
+		});
+	}
+};
+
+export enum LoginOption {
+	SignUp,
+	SignIn,
+	Unselected,
+}
+
+export enum LoginState {
+	Failed_ExistingAccount = 'The entered username is already being used.',
+	Failed_NonExistingAccount = 'The entered username does not belong to any account.',
+	Failed_NoService = 'An error occurred while requesting the service. Please, try later.',
+	Succeed = 'Logged in successfully.',
+	Succeed_SignUp = 'Your account has been created successfully.',
+	Succeed_SignIn = 'Logged in successfully.',
+	NotLogged = 'Login.',
+	Loading = 'Loading.',
+}
+
+export const evalInputRegex = /(^[A-Za-z][A-za-z0-9]{6,9})/;
