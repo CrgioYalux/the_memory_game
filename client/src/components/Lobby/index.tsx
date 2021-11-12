0ca46917b95d4fbad3558709ec51a8da1f27ffd3
Crgio <randaledomman1@gmail.com>
2021-11-12 01:16:25 -0300

    feat(client): create a Lobby component for selecting the game config and launching it

---
+++ client/src/components/Lobby/index.tsx
@@ -0,0 +1,122 @@
+import './Lobby.scss';
+import { useState } from 'react';
+import { usePanel } from '../../providers/PanelProvider';
+import { useClient } from '../../providers/ClientProvider';
+import { formatTime } from '../../hooks/useTime/time';
+import { Game } from '../Game';
+import { Time } from '../../hooks/useTime/time';
+
+interface GameProps {}
+
+const timeOptions: Time[] = [
+	{ minutes: 0, seconds: 30 },
+	{ minutes: 1, seconds: 0 },
+	{ minutes: 1, seconds: 30 },
+];
+const difficultyOptions = [
+	{ difficulty: 3 },
+	{ difficulty: 5 },
+	{ difficulty: 7 },
+];
+
+export const Lobby = ({}: GameProps) => {
+	const [difficulty, setDifficulty] = useState<number>(
+		difficultyOptions[0].difficulty,
+	);
+	const [time, setTime] = useState<Time | string>(timeOptions[0]);
+	const [playing, setPlaying] = useState<boolean>(false);
+	const { goToBase } = usePanel();
+	const { logged } = useClient();
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		setPlaying(true);
+	};
+
+	if (playing) {
+		return <Game difficulty={difficulty} time={time} />;
+	}
+
+	if (!logged) {
+		return (
+			<div className="Lobby-container _not_logged">
+				<span>
+					In order to save your progress when playing, you must use an account.
+				</span>
+				<span>
+					Go and create yours in a second, it's not even a safe thing, you'll
+					only need a username and a nickname.
+				</span>
+				<button onClick={goToBase}>go to login</button>
+			</div>
+		);
+	}
+	return (
+		<div className="Lobby-container _logged">
+			<form onSubmit={handleSubmit}>
+				<div className="game-options">
+					<section>
+						<h3>Difficulty</h3>
+						<div className="difficulty-options">
+							{difficultyOptions.map(({ difficulty: d }, idx) => {
+								return (
+									<div key={idx} className="difficulty-option">
+										<input
+											type="radio"
+											name="difficulty-option"
+											id={`difficulty-option-${idx}`}
+											value={difficulty}
+											onChange={() => setDifficulty(d)}
+											checked={d === difficulty}
+										/>
+										<label
+											htmlFor={`difficulty-option-${idx}`}
+										>{`${d}X${d}`}</label>
+									</div>
+								);
+							})}
+						</div>
+					</section>
+					<section>
+						<h3>Time</h3>
+						<div className="time-options">
+							{timeOptions.map((t, idx) => {
+								return (
+									<div key={idx} className="time-option _normal">
+										<input
+											type="radio"
+											name="time-option"
+											id={`time-option-${idx}`}
+											onChange={() => setTime(t)}
+											value={JSON.stringify(t)}
+											checked={
+												typeof time === 'object' &&
+												t.seconds === time.seconds &&
+												t.minutes === time.minutes
+											}
+										/>
+										<label htmlFor={`time-option-${idx}`}>
+											{formatTime(t)}
+										</label>
+									</div>
+								);
+							})}
+							<div className="time-option _custom">
+								<input
+									type="radio"
+									name="time-option"
+									id="time-option-custom"
+									value="custom"
+									onChange={() => setTime('custom')}
+									checked={time === 'custom'}
+								/>
+								<label htmlFor="time-option-custom">custom</label>
+							</div>
+						</div>
+					</section>
+				</div>
+				<button type="submit">Play</button>
+			</form>
+		</div>
+	);
+};
