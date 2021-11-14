303b407d4b71bca347c64b647da7a786217ecffe
Crgio <randaledomman1@gmail.com>
2021-11-14 05:33:03 -0300

    feat(client): throw everything inside App main component

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Game } from '../Game';
+import { Lobby } from '../Lobby';
 import { Login } from '../Login';
 import { PanelProvider } from '../../providers/PanelProvider';
 import { Scoreboard } from '../Scoreboard';
@@ -8,8 +8,8 @@ import { Navbar } from '../Navbar';
 
 const components = [
 	{
-		name: 'Game',
-		component: <Game />,
+		name: 'Lobby',
+		component: <Lobby />,
 		visibility: false,
 	},
 	{
@@ -23,12 +23,23 @@ const components = [
 		visibility: true,
 	},
 ];
+export enum Section {
+	Login = 'Login',
+	Scoreboard = 'Scoreboard',
+	Lobby = 'Lobby',
+}
 
 export const App = () => {
 	return (
 		<div className="App-container">
 			<ClientProvider>
-				<PanelProvider components={components}>
+				<PanelProvider
+					components={components}
+					baseComponent={{
+						ifLogged: Section.Scoreboard,
+						ifNotLogged: Section.Login,
+					}}
+				>
 					<Navbar components={components} />
 				</PanelProvider>
 			</ClientProvider>
